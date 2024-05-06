4c95734b19fb1e6958127f3e2aa4976926953899
shogo onoda <shogo@example.com>
2024-05-06 00:09:34 +0000

    bit_operation.phpを書いた

---
+++ bit_operation.php
@@ -0,0 +1,17 @@
+<?php
+
+$i = random_int(0, 255);
+$j = random_int(0, 255);
+
+$k = $i & $j;
+printf("%x & %x = %x \n", $i, $j, $k);
+
+$k = $i | $j;
+printf("%x | %x = %x \n", $i, $j, $k);
+
+$k = $i ^ $j;
+printf("%x ^ %x = %x \n", $i, $j, $k);
+
+$k = ~$i;
+printf("~%x = %x \n", $i, $k);
+?>
