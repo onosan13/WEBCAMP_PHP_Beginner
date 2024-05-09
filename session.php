a801a19356a654a4957682005953706d23dfc229
shogo onoda <shogo@example.com>
2024-05-09 07:30:46 +0000

    session.phpを書いた

---
+++ session.php
@@ -0,0 +1,22 @@
+<?php
+
+ob_start();
+
+session_start();
+
+function h(string $s) : string
+{
+    return htmlspecialchars($s, ENT_QUOTES);
+}
+
+$counter=$_SESSION['coounter']?? 1;
+
+echo "セッション)あなたがこのサイトを訪れたのは " , h( strval($counter) ) , "回目ですね。";
+
+$next_counter=$counter+1;
+
+$_SESSION['counter']=$next_counter;
+
+ob_end_flush();
+
+?>
