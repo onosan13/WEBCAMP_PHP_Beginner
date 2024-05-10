aba30487b3be88fc3ffdfb809208e8a90074a942
shogo onoda <shogo@example.com>
2024-05-10 10:32:19 +0000

    exception.phpを書いた

---
+++ exception.php
@@ -0,0 +1,12 @@
+<?php
+
+try{
+    throw new \Exception('自力で投げた例外');
+    $obj= new SplFileObject('dummy');
+    echo "例外が起きた後の処理。ここは通る？ \n";
+}catch(\Throwable $e){
+    echo "例外が発生したらここを通る \n";
+    var_dump($e->getMessage());
+}
+
+echo "fin. \n";
