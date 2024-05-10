2cc31d30cf79c3a093363261a7e379fc2772117d
shogo onoda <shogo@example.com>
2024-05-10 06:13:09 +0000

    class_2.phpを書いた

---
+++ class_2.php
@@ -0,0 +1,19 @@
+<?php
+
+class Myclass
+{
+    const HOGE='string';    
+    
+    public function __construct()
+    {
+        echo "コンストラクタが動いた！ \n";    
+    }
+    
+    public function __destruct(){
+        echo "デストラクタが動いた \n";
+    }
+}
+
+var_dump(Myclass::HOGE);
+
+$obj=new Myclass();
